0bef23cebb68a69104ca5dbcd8e5f30692f3d12f
dandan1232 <345652127@qq.com>
2021-09-23 10:28:39 +0800

    LinearLayout（线性布局）/FrameLayout（框架布局）/RelativeLayout（相对布局）/TableLayout(表格布局)210923

---
+++ app/src/main/java/online/danbao/andoridstudy/LayoutActivity.java
@@ -0,0 +1,15 @@
+package online.danbao.andoridstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_linearlayout);
+        setContentView(R.layout.activity_relativelayout_execrise);
+    }
+}
